80e449d00e63080045ae5490bdf7e27768662efb
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2023-12-20 01:18:20 +0530

    Init & navbar

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+    },},
+  },
+  plugins: [],
+}
